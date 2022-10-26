0638803792d098665b4c60982067a9ab33815f99
Hippies <60807378+Monamoureux@users.noreply.github.com>
2022-10-26 11:49:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -101,6 +101,22 @@ const USER_CONFIG = {
 //         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: 'BB',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'PDU17293Th1n7IMC3I12HIh8OCFTbGhnqG1DKyKQK',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: '0002',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+//       horoscopeDate: '05-12',
+      province: '湖北',
+      // 所在城市或县区
+      city: '武汉',
+      festivals: [],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [],
+    },
   ],
 
 
