466e6f91069e8154c42ad1869734a3f14f34cf89
Hippies <60807378+Monamoureux@users.noreply.github.com>
2022-10-26 11:44:00 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -36,6 +36,39 @@ const TEMPLATE_CONFIG = [
       
       一些小热搜：{{tian_api_network_hot.DATA}}
     `
+  },
+    {
+    id: '0002',
+    title: '深夜偶像歌姬粉丝应援团每日推送',
+    desc: `
+      **{{date.DATA}}**
+      
+      下个休息日：{{holidaytts.DATA}}
+      ---
+      城市：{{city.DATA}}
+      
+      天气：{{weather.DATA}}
+      
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+      
+      预防感冒提醒：{{ganmao.DATA}}
+      
+      {{comprehensive_horoscope.DATA}}
+      
+      {{career_horoscope.DATA}}
+      
+      {{wealth_horoscope.DATA}}
+      
+      {{healthy_horoscope.DATA}}
+      
+      今日文案：{{moment_copyrighting.DATA}}
+      
+      今日鸡汤：{{poison_chicken_soup.DATA}}
+      
+      今日情话：{{earthy_love_words.DATA}}
+      
+      一些小热搜：{{tian_api_network_hot.DATA}}
+    `
   }
 ]
 
