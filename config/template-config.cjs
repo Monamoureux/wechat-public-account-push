73e12e9c855465e2d5a45821acb29f6196ec9e32
Hippies <60807378+Monamoureux@users.noreply.github.com>
2022-10-26 14:52:57 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -3,39 +3,48 @@ const TEMPLATE_CONFIG = [
   {
     id: '0001',
     title: '小蒋今日汇报',
-    desc: `
-      **{{date.DATA}}**
-      
-      下个休息日：{{holidaytts.DATA}}
-      ---
-      城市：{{city.DATA}}
-      
-      天气：{{weather.DATA}}
-      
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
-      预防感冒提醒：{{ganmao.DATA}}
-      
-      {{comprehensive_horoscope.DATA}}
-      
-      {{career_horoscope.DATA}}
-      
-      {{wealth_horoscope.DATA}}
-      
-      {{healthy_horoscope.DATA}}
-      ---
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
-      ---
-      今日文案：{{moment_copyrighting.DATA}}
-      
-      今日鸡汤：{{poison_chicken_soup.DATA}}
-      
-      今日情话：{{earthy_love_words.DATA}}
-      
-      一些小热搜：{{tian_api_network_hot.DATA}}
-    `
+    desc:`
+📆 {{date.DATA}}
+
+👍 下个休息日：{{holidaytts.DATA}}
+
+⛪ 城市：{{city.DATA}}
+
+☁️ 天气：{{weather.DATA}}
+
+⚠ 气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+
+😖 温馨提醒：{{ganmao.DATA}}
+
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+✨ {{comprehensive_horoscope.DATA}}
+
+💼 {{career_horoscope.DATA}}
+
+😍 {{wealth_horoscope.DATA}}
+
+💪 {{healthy_horoscope.DATA}}
+
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+💑 今天是我们相识的第{{love_day.DATA}}天
+
+💖 {{birthday_message.DATA}}
+
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+💬 今日文案：{{moment_copyrighting.DATA}}
+
+👻 今日鸡汤：{{poison_chicken_soup.DATA}}
+
+🌈 今日情话：{{earthy_love_words.DATA}}
+
+~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+
+📣 一些小热搜：
+{{tian_api_network_hot.DATA}}
+`
   },
     {
     id: '0002',
